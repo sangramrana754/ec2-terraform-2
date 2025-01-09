6bbde52c2b07506cedd5b9c01adc020d1a36c035
sangramrana754 <sangramrana754@gmail.com>
2025-01-09 12:27:17 +0530

    nginx

--- main.tf
+++ main.tf
@@ -3,16 +3,24 @@ provider "aws" {
   region = var.aws_region
 }
 
+
 resource "aws_instance" "my_ec2" {
   ami           = "ami-0fd05997b4dff7aac" # Update with the desired AMI ID
   instance_type = "t2.micro"
   key_name      = "srana"    # Update with your EC2 key pair
-#  security_groups = "sg-06b988bef04fb9680" # Update with your security group ID
   subnet_id     = "subnet-098b4c66e13e11f87" # Update with your subnet ID
 
   tags = {
-    Name = "MyEC2GitHub-2"
+    Name = "MyEC2GitHub-3"
   }
+
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo apt update
+    sudo apt install -y nginx
+    sudo systemctl start nginx
+    sudo systemctl enable nginx
+  EOF
 }
 
 output "instance_public_ip" {
